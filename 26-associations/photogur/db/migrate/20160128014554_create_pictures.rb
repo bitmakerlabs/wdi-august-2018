a40b5e7212825653144903c4636d548b156b5485
NatalieBlack <natalievictoriablack@gmail.com>
2018-01-15 11:46:15 -0500

    Update pre-class code for 26-associations

--- 26-associations/photogur/db/migrate/20160128014554_create_pictures.rb
+++ 26-associations/photogur/db/migrate/20160128014554_create_pictures.rb
@@ -1,4 +1,4 @@
-class CreatePictures < ActiveRecord::Migration
+class CreatePictures < ActiveRecord::Migration[5.1]
   def change
     create_table :pictures do |t|
       t.string :url
--- 26-associations/photogur/db/schema.rb
+++ 26-associations/photogur/db/schema.rb
@@ -13,11 +13,11 @@
 ActiveRecord::Schema.define(version: 20160128014554) do
 
   create_table "pictures", force: :cascade do |t|
-    t.string   "url"
-    t.string   "title"
-    t.string   "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.string "url"
+    t.string "title"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
