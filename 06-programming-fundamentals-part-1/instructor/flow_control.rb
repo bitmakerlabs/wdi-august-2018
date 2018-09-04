939357fc41256d934b008d1afa650569352ef686
Daniel Moniz <daniel.moniz@gmail.com>
2018-09-04 13:40:39 -0400

    06-programming-fundamentals - Add examples.

--- 06-programming-fundamentals-part-1/instructor/flow_control.rb
+++ 06-programming-fundamentals-part-1/instructor/flow_control.rb
@@ -6,6 +6,25 @@
 #   then do something else
 
 
+# puts "What's the current temperature?"
+# temperature = gets.to_i
+# if temperature > 20
+#   puts "Shorts and Tshirt day!"
+#   if temperature > 35
+#     puts "But be careful."
+#   end
+# elsif temperature > 10
+#   puts "Wear a sweater."
+# elsif temperature > 0
+#   puts "Wear a coat."
+# else
+#   puts "Bundle up"
+# end
+
+sunny_outside = false
+if sunny_outside
+  puts "It was nice outside!"
+end
 
 
 
@@ -14,6 +33,12 @@
 # for loop
 # while loop
 # until loop
+
+# guesses = 0
+# while guesses < 5
+#   puts "This is the song that never ends..."
+#   guesses += 1
+# end
 
 
 
---
+++ 06-programming-fundamentals-part-1/instructor/hello_world.rb
@@ -0,0 +1,17 @@
+
+# puts "Hello world!"
+
+cars = 0
+
+cars += 3
+
+cars *= 3
+
+puts cars
+
+
+user_input = gets.chomp
+puts "You said, '#{user_input.to_i * 3}'"
+
+
+puts 'got input'
