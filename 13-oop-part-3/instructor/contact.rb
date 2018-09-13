f1869eeccc7b6afb5b31a4e5034bdbdee0a99a9e
bitmakerlabs-room-202 <bitmakerlabs-room-202@bitmaker.co>
2018-09-13 12:01:18 -0400

    [13-oop-part-3] Class functionality of the Contact class

--- 13-oop-part-3/instructor/contact.rb
+++ 13-oop-part-3/instructor/contact.rb
@@ -1,10 +1,32 @@
 class Contact
 
-  def initialize(first_name, last_name, email, notes)
+  @@list = []
+  @@next_id = 1000
+
+  def initialize(first_name, last_name, email, notes = 'N/A')
     @first_name = first_name
     @last_name = last_name
     @email = email
     @notes = notes
+
+    @id = nil
+  end
+
+  def self.all
+    @@list
+  end
+
+  def self.create(first_name, last_name, email, notes = 'N/A')
+    new_contact = self.new(first_name, last_name, email, notes)
+    new_contact.save
+    new_contact
+  end
+
+  def self.find(id)
+    # Left to the student as an exercise
+    # Iterate through the entire list of contacts (@@list)
+    # Ask which ones has that id
+    # Return that instance
   end
 
   # Attribute Readers
@@ -46,6 +68,12 @@ class Contact
   # the instance variables directly OR via their readers
   def full_name
     "#{ @first_name } #{ last_name }"
+  end
+
+  def save
+    @id = @@next_id
+    @@next_id += 1
+    @@list << self
   end
 
 end
--- 13-oop-part-3/instructor/main.rb
+++ 13-oop-part-3/instructor/main.rb
@@ -2,11 +2,18 @@ require_relative 'contact.rb'
 
 clark = Contact.new('Clark', 'Kent', 'clark@dailyplanet.com', 'Goes into the phone booth a lot')
 bruce = Contact.new('Bruce', 'Wayne', 'bruce@wayneenterprises.com', "Doesn't smile very much")
-diana = Contact.new('Diana', 'Prince', 'diana@amazon.com', 'N/A')
-puts clark.first_name
-puts bruce.last_name
+diana = Contact.new('Diana', 'Prince', 'diana@amazon.com')
 
-clark.first_name = 'Bill'
-puts clark.first_name
+dave1 = Contact.create('Dave', 'Wong', 'dave@gmail.com')
+dave2 = Contact.create('Dave', 'Wong', 'dave@hotmail.com')
+dave3 = Contact.create('Dave', 'Wong', 'dave@yahoo.com', 'Copycat')
 
-puts diana.full_name
+Contact.all.each do |contact|
+  puts "[ #{ contact.first_name } #{ contact.last_name } #{ contact.email } #{ contact.notes } ]"
+end
+
+
+puts Contact.all.size
+
+some_dave = Contact.find(1002)
+puts some_dave.inspect
