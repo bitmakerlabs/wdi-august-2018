8bf6d3f1438ff933bba5e25e16a3b8411d378fdd
bitmakerlabs-room-202 <bitmakerlabs-room-202@bitmaker.co>
2018-09-21 10:48:31 -0400

    [18-web-frameworks-part-2] Add dynamic titles

--- 18-web-frameworks-part-2/instructor/cookbook/server.rb
+++ 18-web-frameworks-part-2/instructor/cookbook/server.rb
@@ -5,17 +5,21 @@ get '/' do
 end
 
 get '/about' do
+  @title = 'About Us'
   erb :about
 end
 
 get '/contact' do
+  @title = 'Contact Us'
   erb :contact
 end
 
 get '/legal' do
+  @title = 'Fine Print'
   erb :legal
 end
 
 get '/recipes' do
+  @title = 'Recipes'
   erb :recipes
 end
