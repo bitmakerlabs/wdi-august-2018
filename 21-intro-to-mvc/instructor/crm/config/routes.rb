56d95d0ea1f73eb4f5fa2a883845eb141d7fa4e8
bitmakerlabs-room-202 <bitmakerlabs-room-202@bitmaker.co>
2018-09-25 11:42:02 -0400

    [21-intro-to-mvc] Generate controller with bin/rails g controller Contacts index new create show

--- 21-intro-to-mvc/instructor/crm/config/routes.rb
+++ 21-intro-to-mvc/instructor/crm/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'contacts/index'
+  get 'contacts/new'
+  get 'contacts/create'
+  get 'contacts/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
