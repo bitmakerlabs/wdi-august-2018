6cbb0de99e41d5a491aa052208c7c337bfc82405
bitmakerlabs-room-202 <bitmakerlabs-room-202@bitmaker.co>
2018-09-25 11:37:08 -0400

    [21-intro-to-mvc] Generate Contact class with bin/rails generate model Contact first_name:string last_name:string email:string notes:text; bin/rails db:migrate

---
+++ 21-intro-to-mvc/instructor/crm/db/migrate/20180925153506_create_contacts.rb
@@ -0,0 +1,12 @@
+class CreateContacts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :contacts do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.text :notes
+
+      t.timestamps
+    end
+  end
+end
