580e4fbd121f033e508e0fb9649c62be674e442e
bitmakerlabs-room-202 <bitmakerlabs-room-202@bitmaker.co>
2018-10-01 10:26:03 -0400

    [25-migrations-and-validations] First set of class methods on Track

--- 25-migrations-and-validations/instructor/chinook/app/models/track.rb
+++ 25-migrations-and-validations/instructor/chinook/app/models/track.rb
@@ -1,2 +1,27 @@
 class Track < ApplicationRecord
+
+  SHORT = 180000
+  LONG = 360000
+
+  def self.short
+    shorter_than(SHORT)
+  end
+
+  def self.long
+    longer_than_or_equal_to(LONG)
+  end
+
+  def self.medium
+    longer_than_or_equal_to(SHORT).
+    shorter_than(LONG)
+  end
+
+  def self.shorter_than(length)
+    where('milliseconds < ?', length)
+  end
+
+  def self.longer_than_or_equal_to(length)
+    where('milliseconds >= ?', length)
+  end
+
 end
