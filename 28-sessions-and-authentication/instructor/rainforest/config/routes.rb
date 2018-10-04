e445e502f100318a00e4f05034705f4b075979c8
bitmakerlabs-room-202 <bitmakerlabs-room-202@bitmaker.co>
2018-10-04 11:08:02 -0400

    [28-sessions-and-authentication] Update products index, update routes

--- 28-sessions-and-authentication/instructor/rainforest/config/routes.rb
+++ 28-sessions-and-authentication/instructor/rainforest/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  get 'products/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :products, only: [:index]
+
+  root 'products#index'
 end
