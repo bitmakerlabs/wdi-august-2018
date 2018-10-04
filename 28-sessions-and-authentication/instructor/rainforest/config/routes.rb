afe95447fed57f367b83a44234bf7269e29cb3d5
bitmakerlabs-room-202 <bitmakerlabs-room-202@bitmaker.co>
2018-10-04 11:22:39 -0400

    [28-sessions-and-authentication] Remvoe useless view, update routes to be RESTful

--- 28-sessions-and-authentication/instructor/rainforest/config/routes.rb
+++ 28-sessions-and-authentication/instructor/rainforest/config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :products, only: [:index]
+  resources :products, only: %i(index)
+  resource :users, only: %i(new create)
 
   root 'products#index'
 end
