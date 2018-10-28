857277004b63731ac93d8f09bf0396f20803e7d8
Daniel Moniz <daniel.moniz@gmail.com>
2018-10-28 14:21:30 -0400

    41-ajax-and-json: update migration version.

--- 41-ajax-and-json/instructor/robots/db/migrate/20150717152557_create_robots.rb
+++ 41-ajax-and-json/instructor/robots/db/migrate/20150717152557_create_robots.rb
@@ -1,4 +1,4 @@
-class CreateRobots < ActiveRecord::Migration
+class CreateRobots < ActiveRecord::Migration[5.1]
   def change
     create_table :robots do |t|
       t.string :name
--- 41-ajax-and-json/instructor/robots/db/schema.rb
+++ 41-ajax-and-json/instructor/robots/db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -14,13 +13,13 @@
 ActiveRecord::Schema.define(version: 20150717152557) do
 
   create_table "robots", force: :cascade do |t|
-    t.string   "name"
-    t.string   "address"
-    t.integer  "model_number"
-    t.boolean  "lasers"
-    t.boolean  "japanese"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.string "name"
+    t.string "address"
+    t.integer "model_number"
+    t.boolean "lasers"
+    t.boolean "japanese"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
