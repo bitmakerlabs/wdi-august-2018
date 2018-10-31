d40aa2274ad1ca07187a06f778489455658ad9ae
Daniel Moniz <daniel.moniz@gmail.com>
2018-10-31 12:03:58 -0400

    Merge.

--- 39-events/instructor/delegate.js
+++ 39-events/instructor/delegate.js
@@ -6,10 +6,16 @@ document.addEventListener('DOMContentLoaded', function() {
 
   var container = document.querySelector('#container');
 
+  container.addEventListener('click', function(event) {
+    if(event.target.classList.contains('circle')) {
+      event.target.remove();
+    }
+  });
+
   var boxMaker = document.querySelector('#box-maker');
 
   boxMaker.addEventListener('click', function() {
-      var x = Math.ceil(Math.random() * 400),
+        var x = Math.ceil(Math.random() * 400),
           y = Math.ceil(Math.random() * 200),
           c = Math.ceil(Math.random() * 7);
 
@@ -25,6 +31,23 @@ document.addEventListener('DOMContentLoaded', function() {
 
       container.append(circle);
 
+      // circle.addEventListener('click', function(event) {
+      //   event.target.remove();
+      //   console.log("removed!!");
+      // });
+
       console.log('link was clicked');
   });
+
+
+  // DOES NOT WORK BECAUSE ...
+  // On page load, there are no circles. We can't add "click listener" to circles that don't exist.
+  // Select all `cirlces`, iterate over them (`forEach`), add eventListeners to each circle
+  // document.querySelectorAll('.circle').forEach(function(circle) {
+  //   circle.addEventListener('click', function(event) {
+  //     event.target.remove();
+  //     console.log("removed!!");
+  //   });
+  // });
+
 });
